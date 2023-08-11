e608d80c10eff2765705ddddd06bc9a6c751e2b3
Kongnyu Roger <kongnyuroger711@gmail.com>
2023-08-11 09:51:03 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('connected to mongoose'))
+
+const indexRouter = require('./routes/index')
+
+app.use('/', indexRouter)
+app.listen(process.env.PORT || 3000)
